ffaaabb545244e7dd58acff9303d3622ac856d2a
SamuelF <samuellss14@hotmail.com>
2025-09-09 15:04:43 -0300

    cafeteria

--- src/app/components/Cards.tsx
+++ src/app/components/Cards.tsx
@@ -1,7 +1,11 @@
+'use client'
 import { cardapioCafeteria } from "../data/Produtos";
+import useAlert from "../hooks/useAlerts";
 import "./Cards.css"
+import HeartLike from "./HeartLike";
 
 export default function Cards(){
+    const { ConfirmAlert, doubleAlert } = useAlert();
     return(
         <section className="cards-section">
             <h1 className="cards-title">CARDAPIO CAFETERIA</h1>
@@ -12,11 +16,14 @@ export default function Cards(){
                             <img className="card-image" src={product.image} alt={product.title} />
                         </div>
                         <div className="card-content">
+                            <div className="texto-btn">
+                                    <h3 className="card-title">{product.title}</h3>
+                                    <HeartLike initialCount={product.like} />
+                                </div>
                             <div>
-                                <h3 className="card-title">{product.title}</h3>
                                 <p className="card-description">{product.description}</p>
                             </div>
-                            <button className="card-button">Comprar : {product.price}</button>
+                            <button className="card-button" onClick={()=> ConfirmAlert()}>Comprar : {product.price}</button>
                         </div>
                     </div>
                 ))}
--- src/app/data/Produtos.ts
+++ src/app/data/Produtos.ts
@@ -4,23 +4,27 @@ export const cardapioCafeteria = [
         description: "Cafe espresso com leite cremoso e espumo",
         price: "R$ 22,90",
         image: "cappuccino.png",
+        like: 69
     },
     {
         title: "Mocha",
         description: "Café com chocolate e leite gelado",
         price: "R$ 20,90",
         image: "mocha.png",
+        like: 56
     },
     {
         title: "Pão de Queijo",
         description: "Porção do classico pão de queijo",
         price: "R$ 11,95",
         image: "paoqueijo.png",
+        like: 67
     },
     {
         title: "Bolo de Cenoura",
         description: "Bolo de cenoura com cobertura de chocolate",
         price: "R$ 45,90",
         image: "cenoura.png",
+        like: 64
     },
 ]
